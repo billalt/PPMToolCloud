e0b9b1c1c61d372d1294517225e135dcd337d660
BILLALT <billal.taha@amdocs.com>
2020-02-06 12:09:50 +0200

    react

---
+++ src/main/resources/static/precache-manifest.e0d295a22b23c6d1fd91ddda55c83890.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d808db543114cf2395bfad9abdfa11d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9ffd4201abd0dbfd9fa",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "4c4ed7d938edc870b498",
+    "url": "/static/css/main.e5e8de55.chunk.css"
+  },
+  {
+    "revision": "b9ffd4201abd0dbfd9fa",
+    "url": "/static/js/2.7dd4d75d.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/static/js/2.7dd4d75d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c4ed7d938edc870b498",
+    "url": "/static/js/main.41665b4c.chunk.js"
+  },
+  {
+    "revision": "0e0de284a6b544b8ab0b",
+    "url": "/static/js/runtime-main.df9c3e05.js"
+  }
+]);
